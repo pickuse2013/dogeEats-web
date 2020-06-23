5feaf5ef773d5e39d656e9eb46878327800b2462
pickuse <pickuse2013@gmail.com>
2020-06-23 12:25:54 +0800

    add image

--- database/migrations/2020_06_14_033718_create_restaurants_table.php
+++ database/migrations/2020_06_14_033718_create_restaurants_table.php
@@ -20,15 +20,17 @@ class CreateRestaurantsTable extends Migration
             $table->integer('service_fee')->default('0')->comment('服務費'); // 1-3
             $table->string('address')->nullable()->comment('地址');
             $table->string('position')->nullable()->comment('座標');
+            $table->string('img_url')->comment('url');
             $table->timestamps();
         });
 
         \DB::table('restaurants')->insert(
             [
                 'name' => '肯德基KFC-台北八德餐廳',
-                'description'=> '肯德基是源自美國的快餐連鎖店，總部設於肯塔基州路易維爾市，以炸雞為主力產品。總體來說是全球第二大的餐飲連鎖企業，僅次於麥當勞，截至2015年12月，在123個國家和地區擁有20,000+個分店。目前與必勝客、塔可鐘同為美國跨國餐飲集團百勝旗下子公司。',
+                'description'=> '美式炸雞店',
                 'address' => '100台北市中正區八德路一段64號',
-                'position' => '25.0431988,121.5313617'
+                'position' => '25.0431988,121.5313617',
+                'img_url' => 'https://resource01-proxy.ulifestyle.com.hk/res/v3/image/content/2575000/2578798/BlackDiamondTruffleCOB_PR_LTO_A3_1024.jpg'
             ]
         );
 
@@ -37,9 +39,9 @@ class CreateRestaurantsTable extends Migration
                 'name' => '嵐迪義大利麵館',
                 'description'=> '只是義大利麵',
                 'address' => '100台北市中正區八德路一段82巷9弄1號',
-                'position' => '25.0431988,121.5313617'
-            ]
-        );
+                'position' => "25.0431988,121.5313617",
+                'img_url' => "https://candicecity.com/wp-content/uploads/flickr/21318023508_633fb3d1eb_b.jpg"
+            ]);
     }
 
     /**
