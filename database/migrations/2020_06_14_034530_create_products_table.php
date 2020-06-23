3f71d55406e335fa4595a3184df206af73d62965
pickuse <pickuse2013@gmail.com>
2020-06-23 12:40:52 +0800

    add image

--- database/migrations/2020_06_14_034530_create_products_table.php
+++ database/migrations/2020_06_14_034530_create_products_table.php
@@ -29,25 +29,29 @@ class CreateProductsTable extends Migration
                 'category_id' => '1',
                 'name' => '標準大雞排',
                 'description' => '新鮮的炸雞排',
-                'price' => '100'
+                'price' => '100',
+                'image_url' => 'https://media.zenfs.com/en/yahoo__216/19c7202e0e02977ff82b28f3803b1d96'
             ],
             [
                 'category_id' => '1',
                 'name' => '標準小雞排',
                 'description' => '新鮮的炸雞排',
-                'price' => '100'
+                'price' => '100',
+                'image_url' => 'https://media.zenfs.com/en/yahoo__216/19c7202e0e02977ff82b28f3803b1d96'
             ],
             [
                 'category_id' => '2',
                 'name' => '凱薩沙拉',
                 'description' => '油膩食品的配角，可以讓你稍微活著久一點',
-                'price' => '70'
+                'price' => '70',
+                'image_url' => 'https://pic.pimg.tw/anthony0520/1438119347-2549514926_n.jpg'
             ],
             [
                 'category_id' => '3',
                 'name' => '奶油白醬麵',
                 'description' => '起司粉可能不用錢',
-                'price' => '110'
+                'price' => '110',
+                'image_url' => 'https://cdn.mamaclub.com/wp-content/uploads/2019/04/20190420_s0601-810x424.jpg'
             ]
         ]);
     }
